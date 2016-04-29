2429e93ee2c9fe47307dfce8bed5df83b0dc6f63
LeonardCC <leonard.chesca@gmail.com>
2016-04-29 03:14:23 -0700

    Seeting up Angular

--- ToDoList/js/controllers/TodoCtrl.js
+++ ToDoList/js/controllers/TodoCtrl.js
@@ -1,3 +1,9 @@
-app.controller('TodoCtrl', ['$scope'], function($scope)) {
-	$scope.todolists = []
-}
+app.controller('TodoCtrl', ['$scope', function($scope) { 
+	$scope.todolists = [];
+	$scope.HelloWorld = "Hello World!"
+
+	$scope.addTodoList = function (){
+		$scope.todolists.push({text:$scope.formTodoText, done:false});
+		$scope.formTodoText = '';
+	};
+}]);
