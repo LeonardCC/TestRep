135dd76be9cb0abe66258ee29a17ede7e67b7593
LeonardCC <leonard.chesca@gmail.com>
2016-04-29 04:19:50 -0700

    Add list rules updated. Clear completed added

--- ToDoList/js/controllers/TodoCtrl.js
+++ ToDoList/js/controllers/TodoCtrl.js
@@ -3,7 +3,20 @@ app.controller('TodoCtrl', ['$scope', function($scope) {
 	$scope.HelloWorld = "Hello World!"
 
 	$scope.addTodoList = function (){
+		if ($scope.formTodoText != undefined && $scope.formTodoText != '')
+		{
 		$scope.todolists.push({text:$scope.formTodoText, done:false});
 		$scope.formTodoText = '';
+		}
+		else
+		{
+			alert('List name cannot be empty.')
+		}
+	};
+
+	$scope.removeCompleted = function () {
+		$scope.todolists = _.filter($scope.todolists, function(todolist){
+			return !todolist.done;
+		});
 	};
 }]);
